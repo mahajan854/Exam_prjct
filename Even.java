4ca59ce138020e03b693527311862cc9bcb53adf
mahajan854 <97273501+mahajan854@users.noreply.github.com>
2023-05-04 22:22:02 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,16 @@
+ class Ques8 {
+	public static void main(String[]args)
+	{
+		
+			int num=12;
+			if(num%2==0)
+			{
+				System.out.println("It is even");
+			}else
+			{
+				System.out.println("It is odd");
+			}
+		
+	}
+
+}
